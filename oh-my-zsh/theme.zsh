761195239090cb00fc48e4ab37c413581ed90629
Kamiel Wanrooij <kamiel.wanrooij@gmail.com>
2016-03-31 17:43:28 +0200

    Randomly pick a theme using a logarithmic scale

--- oh-my-zsh/theme.zsh
+++ oh-my-zsh/theme.zsh
@@ -1,5 +1,18 @@
-themes=("nanotech" "pure" "clean" "eastwood" "skaro" "kolo" "amuse" "bureau" "sorin" "terminalparty" "avit" "wezm" "ys")
+# Let's make a log2 function!
+log(){
+  local x=$1 n=2 l=-1
+  if [ "$2" != "" ]; then
+    n=$x
+    x=$2
+  fi
+  while ((x)); do let l+=1 x/=n;done
+  echo $l
+}
+themes=("nanotech" "kolo" "pure" "clean" "skaro" "amuse" "bureau" "sorin" "terminalparty" "avit" "wezm" "ys")
+
 N=${#themes[@]}
-((N=(RANDOM%N)+1))
+let "RND=$RANDOM % 2**$N"
+IDX=$(log RND)
+let "N=$N-$IDX"
 ZSH_THEME=${themes[$N]}
 echo "Using theme $ZSH_THEME"
